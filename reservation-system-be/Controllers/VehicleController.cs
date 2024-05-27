cf0a53a6ce9ad85c7e6e7faf2f6a88757dde97e7
Buddika Kasun <buddikakasun80@gmail.com>
2024-05-28 01:23:58 +0530

    Add files via upload

---
+++ reservation-system-be/Controllers/VehicleController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reservation_system_be.Data;
+using reservation_system_be.DTOs;
+using reservation_system_be.Models;
+using reservation_system_be.Services.VehicleServices;
+
+namespace reservation_system_be.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IVehicleService _vehicleService;
+
+        public VehicleController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<VehicleDto>>> GetAllVehicles()
+        {
+            var vehicles = await _vehicleService.GetAllVehicles();
+            return Ok(vehicles);
+        }
+       
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VehicleDto>> GetVehicle(int id)
+        {
+            try
+            {
+                return await _vehicleService.GetVehicle(id);
+            }
+            catch (DataNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Vehicle>> CreateVehicle(Vehicle vehicle)
+        {
+            return await _vehicleService.CreateVehicle(vehicle);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Vehicle>> UpdateVehicle(int id, Vehicle vehicle)
+        {
+            try
+            {
+                return await _vehicleService.UpdateVehicle(id, vehicle);
+            }
+            catch (DataNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteVehicle(int id)
+        {
+            try
+            {
+                await _vehicleService.DeleteVehicle(id);
+                return Ok();
+            }
+            catch (DataNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<List<Vehicle>>> SearchVehicle(String search)
+        {
+            try
+            {
+                return await _vehicleService.SearchVehicle(search);
+            }
+            catch (DataNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
